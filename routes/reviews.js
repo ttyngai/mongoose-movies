62ce924feb1f995e5a115df01d10226a4c3b10dc
cogilvy <chrisogilvy@cox.net>
2021-12-09 10:57:47 -0800

    Create Review Form

---
+++ routes/reviews.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+// const reviewsCtrl = require('../controllers/reviews');
+
+// POST "/movies/:id/reviews" - Create Review Route
+
+
+
+
+
+module.exports = router;
