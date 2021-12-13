4196654f278bc33054b6688ea1113fe241df5458
cogilvy <chrisogilvy@cox.net>
2021-12-13 11:03:17 -0800

    Final Code for JS Promises

--- seeds.js
+++ seeds.js
@@ -61,6 +61,7 @@ const data = require('./data');
 const p1 = Movie.deleteMany({});
 const p2 = Performer.deleteMany({});
 
+
 Promise.all([p1, p2])
 .then(function(results) {
   console.log(results);
@@ -70,6 +71,15 @@ Promise.all([p1, p2])
   return Movie.create(data.movies);
 }).then(function(movies) {
   console.log(movies);
+  return Promise.all([
+    Performer.findOne({ name: "Mark Hamill" }),
+    Movie.findOne({ title: "Star Wars - A New Hope" }),
+  ]);
+}).then(function(results) {
+  const mark = results[0];
+  const starWars = results[1];
+  starWars.cast.push(mark);
+  return starWars.save();
 }).then(function() {
   process.exit();
 });
