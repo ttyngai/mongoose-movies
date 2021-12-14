7cc74cb598f8d41790ac0a534585b01e98815089
cogilvy <chrisogilvy@cox.net>
2021-12-14 15:36:35 -0800

    Update reviewSchema

--- models/movie.js
+++ models/movie.js
@@ -2,13 +2,16 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
-  content: String,
+  content: {type: String, required: true},
   rating: {
     type: Number,
     min: 1,
     max: 5,
     default: 5
-  }
+  },
+  user: {type: Schema.Types.ObjectId, ref: 'User'},
+  userName: String,
+  userAvatar: String
 }, {
   timestamps: true
 });
