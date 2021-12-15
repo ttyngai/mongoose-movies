3d7605815d9311d4648a3db71e50f4a8f4b22e83
cogilvy <chrisogilvy@cox.net>
2021-12-14 16:15:21 -0800

    ALmost theregit add -A git push

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,8 +1,32 @@
 const Movie = require('../models/movie');
 
 module.exports = {
-  create
+  create,
+  delete: deleteReview
 };
+
+function deleteReview(req, res, next) {
+  // Note the cool "dot" syntax to query on the property of a subdoc
+  Movie.findOne({ "reviews._id": req.params.id }).then(function (movie) {
+    // Find the review subdoc using the id method on Mongoose arrays
+    const review = movie.reviews.id(req.params.id);
+    // Ensure that the review was created by the logged in user
+    if (!review.user.equals(req.user._id)) return res.redirect(`/movies/${movie._id}`);
+    // Remove the review using the remove method of the subdoc
+    review.remove();
+    // Save the updated movie
+    movie.save()
+      .then(function () {
+        // Redirect back to the movie's show view
+        res.redirect(`/movies/${movie._id}`);
+      })
+      .catch(function (err) {
+        // Let Express display an error
+        return next(err);
+        // res.redirect(`/movies/${movie._id}`);
+      });
+  });
+}
 
 function create(req, res) {
   // First find the movie we are adding a review to
--- routes/reviews.js
+++ routes/reviews.js
@@ -7,7 +7,8 @@ const reviewsCtrl = require('../controllers/reviews');
 // POST "/movies/:id/reviews" - Create Review Route
 router.post('/movies/:id/reviews', reviewsCtrl.create);
 
-
+// DELETE "/reviews/:id" - Delete Review Route
+router.delete('/reviews/:id', reviewsCtrl.delete);
 
 
 module.exports = router;
